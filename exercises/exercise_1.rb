ad5879df120e1de9ca844c297646d4b83e2c79c6
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-05-01 12:13:24 -0700

    Finish excercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,6 +5,6 @@ puts "----------"
 
 # Your code goes below here ...
 
-burnaby = Store.create(annual_revenue: 30000, womens_apparel: true, mens_apparel: true)
-richmond = Store.create(annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
-gastown = Store.create(annual_revenue: 190000, womens_apparel: false, mens_apparel: true)
+burnaby = Store.create(name: "Burnaby", annual_revenue: 30000, womens_apparel: true, mens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, womens_apparel: false, mens_apparel: true)
