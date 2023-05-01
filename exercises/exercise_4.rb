791cd7f86a4e37aff1ecabfe95f16a0e82d3a0a5
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-05-01 12:49:51 -0700

    Finish excercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -1,9 +1,26 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
 
 puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true, mens_apparel: false)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: false)
+
+# Store.where(mens_apparel: true)
+@mens_stores = Store.where(womens_apparel: true, annual_revenue: 0..999999)
+
+@mens_stores.each do |store|
+  puts store.name
+end
+
+@womens_stores = Store.where(mens_apparel: true, annual_revenue: 0..999999)
+
+@womens_stores.each do |store|
+  puts store.name
+end
