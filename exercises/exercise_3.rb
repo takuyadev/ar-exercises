4d2fa1acbf607ea65ac7feef0534d6f076655c5d
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-05-01 12:15:20 -0700

    Finish excercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,10 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
 
 puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+Store.destroy(3)
