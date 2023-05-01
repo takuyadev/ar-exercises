3b3a123814502d8cfbd129f9e3990c0acb8e8659
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-05-01 12:59:54 -0700

    Finish excercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,14 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
 
 puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts Store.sum(:annual_revenue)
+puts Store.average(:annual_revenue)
+puts Store.where("1000000 < annual_revenue").count()
